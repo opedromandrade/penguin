48faf99a8bec873fbabe1db01f789f85d2754464
pedro andrade <43356335+opedromandrade@users.noreply.github.com>
2025-11-07 17:50:21 +0000

    update

--- UBUNTU/motherfossa.sh
+++ UBUNTU/motherfossa.sh
@@ -19,13 +19,13 @@ sudo apt update && sudo apt upgrade -y
 #sudo apt-get install i965-va-driver-shaders intel-media-va-driver-non-free intel-gpu-tools
 
 ## Small things
-# Open JDK
-sudo apt-get install openjdk-11-jre
+# Open JDK - open source implementation of the Java Platform
+sudo apt-get install openjdk-21-jre
 
-# GIT -  source-code management tool
+# GIT - open source-code management tool
 sudo apt-get install git 
 
-# Synaptic
+# Synaptic package manager - a graphical package management program for apt
 sudo apt-get install synaptic
 
 ## Some extra zing to Ubuntu
@@ -43,17 +43,17 @@ sudo apt-get install mpv
 #sudo apt-get install totem
 
 ## Audio files software
-# Quodlibet and exfalso
+# Quodlibet and exfalso - a cross-platform audio/music management program
 sudo apt-get install quodlibet exfalso
 
-# EasyTAG
+# EasyTAG - an utility for viewing, editing and writing meta info of different audio files
 sudo apt-get install easytag
 
 ## Book stuff
-# Calibre - https://calibre-ebook.com
+# Calibre - the one stop solution for all  e-book needs - https://calibre-ebook.com
 sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
 
-# Sigil
+# Sigil - EPUB Editor
 sudo apt-get install sigil
 
 ## Aditional formats for compression
@@ -70,15 +70,6 @@ sudo apt-get install rapid-photo-downloader
 # Darktable - darktable is an open source photography workflow application and raw developer
 #sudo apt-get install darktable
 
-## Video and Audio creation
-# Kdenlive
-# add the ppa:
-sudo add-apt-repository ppa:kdenlive/kdenlive-stable && sudo apt update
-# Intsall the mofo
-sudo apt-get install kdenlive
-# Remove KDE Connect
-sudo apt remove kdeconnect
-
 # Audacity
 sudo apt-get install audacity
 
@@ -92,10 +83,11 @@ sudo apt-get install libreoffice-gnome libreoffice
 sudo apt-get install myspell-pt-pt hyphen-pt-pt libreoffice-l10n-pt mythes-pt-pt libreoffice-help-pt
 
 ## Internet Stuff
-# Extra browser [Chromium]
-sudo apt-get install chromium-browser
+# Extra browser [Chromium] - Chromium is an open-source browser project that aims to build a safer, faster, and more stable way for all users to experience the web
+#sudo apt-get install chromium-browser
 
 # FTP support
+# FileZilla - the free FTP solution
 #sudo apt-get install filezilla
 
 # p2p
@@ -106,9 +98,11 @@ sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable && sudo apt-get 
 sudo apt-get remove transmission-gtk
 
 # Firewall
-sudo apt-get install gufw
+# GUFW - A graphical user interface for Ubuntu's Uncomplicated Firewall
+sudo apt-get install ufw gufw
 
 # Handy tools
+# net-tools - a package of legacy Linux command-line utilities for network administration
 sudo apt-get install net-tools
 
 # VPN stuff
